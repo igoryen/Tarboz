e374bde18758586202dd397554171ff1b2829066
Shuming Lin <kevin.shuminglin@gmail.com>
2014-09-28 23:35:01 -0400

    issue#137 - template and other pages issues

--- html_codes_to_use/Shared/footer.php
+++ html_codes_to_use/Shared/footer.php
@@ -1,9 +1,9 @@
  
       <div id="footer">
 	      	<div>
-	      		<a href="/Tarboz/Views/Home/Index.php">Home</a> 
-	         	<a href="/Tarboz/Views/Home/Result.php">About Us</a> 
-	          	<a href="/Tarboz/Views/Entry/Index.php">Contact Us</a>    
+	      		<a href="/Tarboz/Index.php">Home</a> 
+	         	<a href="/Tarboz/html_codes_to_use/Home/Result.php">About Us</a> 
+	          	<a href="/Tarboz/html_codes_to_use/Entry/Index.php">Contact Us</a>    
 	        </div>
 	        <br />  
 	        <div><b>Copyright © 2014 Tarboz.com all rights reserved.<b><div>
