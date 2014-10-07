e1cd3bcdb9d0168af9b5f77124afb89cf83f4d42
igoryen <igor.yentaltsev@gmail.com>
2014-10-07 11:35:30 -0400

    Formatted BusinessLogicTier/

--- classes/BusinessLogicTier/user_login/UserLogin.php
+++ classes/BusinessLogicTier/user_login/UserLogin.php
@@ -1,25 +1,26 @@
-<?php 
+<?php
 
-class UserLogin extends User{
+class UserLogin extends User {
 
   private $username;
   private $password;
-    
+
   public function getLogin() {
-    return $this->usr_login ;
+    return $this->usr_login;
   }
-  
+
   public function setLogin($login) {
-    $this->usr_login  = $login;
+    $this->usr_login = $login;
   }
 
   public function getPassword() {
-    return $this->usr_password ;
+    return $this->usr_password;
   }
 
   public function setPassword($password) {
-    $this->usr_password  = $password;
+    $this->usr_password = $password;
   }
-  
+
 }
+
 ?>
