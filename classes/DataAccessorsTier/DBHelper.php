a02384fec601a05a746bbd0e6402ababf358df72
igoryen <igor.yentaltsev@gmail.com>
2014-10-07 11:45:03 -0400

    issue#187 - formatted DataAccessorsTier

--- classes/DataAccessorsTier/DBHelper.php
+++ classes/DataAccessorsTier/DBHelper.php
@@ -1,66 +1,73 @@
 <?php
-  require_once DB_CONNECTION . 'constants.php';
 
-  class DBHelper{   
-    private $connection;
-    /*-----------------------------------------------------
-      Create a database connection to connect to a db server
-      and select a database to use
-      ----------------------------------------------------*/
-    public function getConnection(){
-      return $this->connection;
-    }
+require_once DB_CONNECTION . 'constants.php';
 
-    private function connectToDB(){
-      // Step 1. Create a Database connection
-      $this->connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS,DB_NAME) 
-        or die("Error, failed to connect" . mysqli_error($this->connection));      
-        //$db_select = mysqli_select_db("prj666", $this->connection) 
-        // or die("Can not connect to the database".mysqli_error($this->connection));
+class DBHelper {
+
+  private $connection;
+
+  /* -----------------------------------------------------
+    Create a database connection to connect to a db server
+    and select a database to use
+    ---------------------------------------------------- */
+
+  public function getConnection() {
+    return $this->connection;
+  }
+
+  private function connectToDB() {
+    // Step 1. Create a Database connection
+    $this->connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME)
+            or die("Error, failed to connect" . mysqli_error($this->connection));
+    //$db_select = mysqli_select_db("prj666", $this->connection)
+    // or die("Can not connect to the database".mysqli_error($this->connection));
+  }
+
+  /* ------------------------------------
+    Close database connection
+    --------------------------------- */
+
+  private function closeConnection() {
+    // Step 5. Close connection
+    if (isset($this->connection)) {
+      mysqli_close($this->connection);
     }
-    
-    /*------------------------------------
-      Close database connection
-      ---------------------------------*/
-    private function closeConnection(){
-      // Step 5. Close connection
-      if(isset($this->connection)){
-        mysqli_close($this->connection);
-      }
-    }
-    
-    public function executeSelect($sql){
-      $this->connectToDB();
+  }
+
+  public function executeSelect($sql) {
+    $this->connectToDB();
+    $result = mysqli_query($this->connection, $sql);
+    $this->closeConnection();
+    return $result;
+  }
+
+  public function executeQuery($sql) {
+    $this->connectToDB();
+    if ($this->connection) {
       $result = mysqli_query($this->connection, $sql);
-      $this->closeConnection();
-      return $result;
     }
-    
-    public function executeQuery($sql){
-      $this->connectToDB();
-      if($this->connection){
-        $result = mysqli_query($this->connection, $sql);
-      }
-      $this->closeConnection();
-      return $result;
-    }
-    
-    // returns last inserted id
-    public function executeInsertQuery($sql){
-      $this->connectToDB();
-      $result = mysqli_query($this->connection, $sql);
-      $last_inserted_id = mysqli_insert_id($this->connection);
-      $this->closeConnection();
-      return $last_inserted_id;
-    }    
-    
-    public function getNumOfRows($sql){
-      $this->connectToDB();
-      $result = mysqli_query($this->connection, $sql);
-      $numOfRows = mysqli_num_rows($result);
-      $this->closeConnection();
-      return $numOfRows;
-    }  
-  
-  } // DBHelper
+    $this->closeConnection();
+    return $result;
+  }
+
+  // returns last inserted id
+  public function executeInsertQuery($sql) {
+    $this->connectToDB();
+    $result = mysqli_query($this->connection, $sql);
+    $last_inserted_id = mysqli_insert_id($this->connection);
+    $this->closeConnection();
+    return $last_inserted_id;
+  }
+
+  public function getNumOfRows($sql) {
+    $this->connectToDB();
+    $result = mysqli_query($this->connection, $sql);
+    $numOfRows = mysqli_num_rows($result);
+    $this->closeConnection();
+    return $numOfRows;
+  }
+
+}
+
+// DBHelper
 ?>
--- classes/DataAccessorsTier/user_login/UserLoginDataAccessor.php
+++ classes/DataAccessorsTier/user_login/UserLoginDataAccessor.php
@@ -1,123 +1,103 @@
 <?php
 
-  require_once DB_CONNECTION . 'DBHelper.php';
+require_once DB_CONNECTION . 'DBHelper.php';
+require_once BUSINESS_DIR_USER . 'User.php';
+//has constants for the table; USER
+require_once(DB_CONNECTION . 'datainfo.php');
 
-  require_once BUSINESS_DIR_USER. 'User.php';
-
-    //has constants for the table; USER
-  require_once(DB_CONNECTION.'datainfo.php'); 
-  //require_once('../../plug-in/email/email_password.php');
-
-
-  class UserLoginDataAccessor {
-    
-    /*-------------------------------
-      for search
-      20. create new instance of database helper
-      30. pass a query statment and get the data
-      40. returns the user
-      ------------------------------*/
-    public function user_Login($loginid, $pwd){
-
-      $user = new User();
-
-      $query = "SELECT * FROM ". USER . " WHERE usr_login =  '$loginid' and usr_password = '$pwd'";
-
-      $dbHelper = new DBHelper(); // 20
-
-      $result = $dbHelper->executeSelect($query); // 30    
-
-      $user = $this->getUser($result);   
-
-      return $user;
-    }
+//require_once('../../plug-in/email/email_password.php');
 
 
-    public function ForgotPassword($email){
+class UserLoginDataAccessor {
+  /* -------------------------------
+    for search
+    20. create new instance of database helper
+    30. pass a query statment and get the data
+    40. returns the user
+    ------------------------------ */
 
-      $reset=false;
+  public function user_Login($loginid, $pwd) {
+    $user = new User();
 
-      $random_text="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@.()";
+    $query = "SELECT * FROM " . USER . " WHERE usr_login =  '$loginid' and usr_password = '$pwd'";
 
-      //TO SHUFFLE THE ABOVE TEXT AND PIC 10 CHARS.
-      $substr = substr(str_shuffle($random_text),0,10);
-
-      $user = new User();
-
-      $query = "SELECT * FROM ". USER . "  WHERE usr_login =  '$email' " ;
-
-      $dbHelper = new DBHelper(); 
-
-      $result = $dbHelper->executeSelect($query); // 30    
-
-      $user = $this->getUser($result);
-
-      $userid=$user->getUserId();
-      $username=$user->getFirstName();
-      $date = date("Y/m/d");
-
-      if($user){
-      //PASSWORD_RESET
-      if($this->email($email,$username,$substr)){
-
-      $query_insert = "INSERT INTO PASSWORD_RESET VALUES('','$substr','$userid', '$date',0)";    
-
-      $dbHelper = new DBHelper();
-
-      $result = $dbHelper->executeQuery($query_insert);
-      if($result) echo "Inserted";
-       }
-      //$last_inserted_id = mysql_insert_id();
-      }
-
-
-
-
-      return $reset;
-    }
-    
-    /*-------------------------------
-      To be moved to bottom later, private to be accessed by member functions only
-      ------------------------------*/
-    private function getUser($selectResult){ 
-       
-      $User = new User();
-      $count = 0;
-      
-      while($list = mysqli_fetch_assoc($selectResult)){
-          
-        $User->setUserId($list['usr_user_id']);     
-        $User->setFirstName($list['usr_first_name']);     
-        $User->setLastName($list['usr_last_name']);     
-        $User->setLogin($list['usr_login']);      
-        //$User->setUserRatingId($list['']);      
-        $User->setEmail($list['usr_email']);      
-        $User->setDOB($list['usr_DOB']);      
-        //$User->setLocation($list['']);      
-        $User->setRegistration_date($list['usr_registration_date']);      
-        $User->setUserType($list['usr_user_type_id']);      
-        $User->setUserLanguage($list['usr_language']);      
-        $User->setEmailSub($list['usr_email_subscribed']);      
-      }
-
-      return $User;  
-    } // end of getUser
-
-
-  private function email($email,$username,$reset_code){
-    ini_set("SMTP","aspmx.l.google.com");
-
-    $subject="Password Reset Request @ Tarboz.com";
-    $sent = false;
-    $message="Dear ".$username."<br>A request has been made for resetting your password, if it was requested by you, please follow the link below and a 
-    a your new password will be sent to your email.<br> Link:".PASSWORD_RESET_LOC."?security= ".$reset_code."<br><br>Tarboz Team";
-
-    mail($email,$subject,$message);
-
- }
-
-
+    $dbHelper = new DBHelper(); // 20
+    $result = $dbHelper->executeSelect($query); // 30
+    $user = $this->getUser($result);
+    return $user;
   }
 
+  public function ForgotPassword($email) {
+    $reset = false;
+    $random_text = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@.()";
+    //TO SHUFFLE THE ABOVE TEXT AND PIC 10 CHARS.
+    $substr = substr(str_shuffle($random_text), 0, 10);
+    $user = new User();
+    $query = "SELECT * FROM " . USER . "  WHERE usr_login =  '$email' ";
+    $dbHelper = new DBHelper();
+    $result = $dbHelper->executeSelect($query); // 30
+    $user = $this->getUser($result);
+    $userid = $user->getUserId();
+    $username = $user->getFirstName();
+    $date = date("Y/m/d");
+
+    if ($user) {
+      //PASSWORD_RESET
+      if ($this->email($email, $username, $substr)) {
+        $query_insert = "INSERT INTO PASSWORD_RESET VALUES('','$substr','$userid', '$date',0)";
+        $dbHelper = new DBHelper();
+        $result = $dbHelper->executeQuery($query_insert);
+        if ($result)
+          echo "Inserted";
+      }
+      //$last_inserted_id = mysql_insert_id();
+    }
+
+
+    return $reset;
+  }
+
+  /* -------------------------------
+    To be moved to bottom later, private to be accessed by member functions only
+    ------------------------------ */
+
+  private function getUser($selectResult) {
+
+    $User = new User();
+    $count = 0;
+
+    while ($list = mysqli_fetch_assoc($selectResult)) {
+
+      $User->setUserId($list['usr_user_id']);
+      $User->setFirstName($list['usr_first_name']);
+      $User->setLastName($list['usr_last_name']);
+      $User->setLogin($list['usr_login']);
+      //$User->setUserRatingId($list['']);
+      $User->setEmail($list['usr_email']);
+      $User->setDOB($list['usr_DOB']);
+      //$User->setLocation($list['']);
+      $User->setRegistration_date($list['usr_registration_date']);
+      $User->setUserType($list['usr_user_type_id']);
+      $User->setUserLanguage($list['usr_language']);
+      $User->setEmailSub($list['usr_email_subscribed']);
+    }
+
+    return $User;
+  }
+
+// end of getUser
+
+  private function email($email, $username, $reset_code) {
+    ini_set("SMTP", "aspmx.l.google.com");
+
+    $subject = "Password Reset Request @ Tarboz.com";
+    $sent = false;
+    $message = "Dear " . $username . "<br>A request has been made for resetting your password, if it was requested by you, please follow the link below and a
+    a your new password will be sent to your email.<br> Link:" . PASSWORD_RESET_LOC . "?security= " . $reset_code . "<br><br>Tarboz Team";
+
+    mail($email, $subject, $message);
+  }
+
+}
 
 ?>
