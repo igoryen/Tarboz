00466f7aa8a8eff1f25cb349bc34b0827f7da555
igoryen <igor.yentaltsev@gmail.com>
2014-10-07 11:56:12 -0400

    issue#191 - formatted user_test/

--- user_test/login.php
+++ user_test/login.php
@@ -1,29 +1,31 @@
-    	<?php
-			// Include utility files
-			require_once '../config.php';	
-			require_once BUSINESS_DIR_USER. 'User.php';	
-			require_once BUSINESS_DIR_USER_LOGIN . 'UserLoginManager.php';
+<?php
 
-				//$_POST['userid']="johnsmith";
-				//$_POST['password']="habib";
+// Include utility files
+require_once '../config.php';
+require_once BUSINESS_DIR_USER . 'User.php';
+require_once BUSINESS_DIR_USER_LOGIN . 'UserLoginManager.php';
+
+//$_POST['userid']="johnsmith";
+//$_POST['password']="habib";
 
 
-				$userid=isset($_POST['userid']) ? $_POST['userid'] : "";
-				$pwd=isset($_POST['password']) ? $_POST['password'] : "";
+$userid = isset($_POST['userid']) ? $_POST['userid'] : "";
+$pwd = isset($_POST['password']) ? $_POST['password'] : "";
 
-					echo $userid."Testing ";
+echo $userid . "Testing ";
 
-				$userManager = new UserLoginManager();
+$userManager = new UserLoginManager();
 
-				$logged = $userManager->userLogin($userid,$pwd);
+$logged = $userManager->userLogin($userid, $pwd);
 
-			//	echo $logged->getFirstName();	
-				
-				if($logged->getFirstName()!=""){
-					echo SUCCESS;
-				}else{  echo FAIL; }
+//	echo $logged->getFirstName();
+
+if ($logged->getFirstName() != "") {
+  echo SUCCESS;
+}
+else {
+  echo FAIL;
+}
 
 //				echo "\nThe user is:".$_SESSION["user"]->getFirstName();
-
-													
-			?>
+?>
