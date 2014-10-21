5ea51e7d5f4eb7328c1c723abf8e2b65758fc325
igoryen <igor.yentaltsev@gmail.com>
2014-10-21 13:00:49 -0400

    issue#262 - moved the debugging table

--- views2/Entry/index.php
+++ views2/Entry/index.php
@@ -1,6 +1,7 @@
   <?php require("../Shared/header.php");
   
   include "../../config.php";
+  include "lib.php";
   require_once BUSINESS_DIR_ENTRY . "EntryManager.php";
   require_once BUSINESS_DIR_ENTRY . "Entry.php";
 
@@ -59,42 +60,7 @@ $translator = $entry->getEntryAuthorId();
     //$user = "<a href='/Tarboz/Views/User/Index.php'>user123</a>";
 $user = $entry->getEntryUserId();
 
-////====== DEBUGGING TABLE====================================================
-//echo "<div style='background-color:white; width: 700px;'>";
-//$d = '';
-//$d .= "<table class=\'debug_table\'>";
-//$d .= '<tr><td>$entryId</td><td>' . $entryId . '</td></tr>';
-//$d .= '<tr><td>isset($entry)</td><td>'
-//        . (isset($entry) ? '1 (set)' : '0 (not set)') . '</td></tr>';
-//
-//$d .= '<tr><td>!is_null($entry)</td><td>'
-//        . (!is_null($entry) ? '1 (not null)' : '0 (null)') . '</td></tr>';
-//
-//$entry_ary = (array) $entry;
-//$d .= '<tr><td>!empty($entry_ary)</td><td>'
-//        . (!empty($entry_ary) ? '1 (not empty)' : '0 (empty)') . '</td></tr>';
-//
-//$properties_of_entry = array_filter(get_object_vars($entry));
-//$d .= '<tr><td>!empty($properties_of_entry)</td><td>'
-//        . (!empty($properties_of_entry) ? '1 (not empty)' : '0 (empty)') . '</td></tr>';
-//
-//$d .= '<tr><td>!null == $entry->getEntryId()</td><td>'
-//        . (!null == $entry->getEntryId() ? '1 (not null)' : '0 (null)') . '</td></tr>';
-//
-//$d .= '<tr><td>!null == $entry->getEntryText()</td><td>'
-//        . (!null == $entry->getEntryText() ? '1 (not null)' : '0 (null)') . '</td></tr>';
-//
-//$d .= '<tr><td>$entry->getEntryId()</td><td>'
-//        . substr($entry->getEntryId(), 0, 25) . '</td></tr>';
-//
-//$d .= '<tr><td>$entry->getEntryText()</td><td>'
-//        . substr($entry->getEntryText(), 0, 25) . '</td></tr>';
-//
-//$d .= '</table>';
-//echo $d;
-//echo "</div>";
-//// ======== END OF DEBUGGING TABLE ==========================================
-
+table_to_see_entry_values($entry);
 
   ?>
 
