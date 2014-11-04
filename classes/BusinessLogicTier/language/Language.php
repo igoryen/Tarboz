b154650c813f024a5b4fb360699a8b231959c9ef
igoryen <igor.yentaltsev@gmail.com>
2014-11-03 19:12:15 -0500

    issue#405 - added a DDL of languages to entryview.php

---
+++ classes/BusinessLogicTier/language/Language.php
@@ -0,0 +1,13 @@
+<?php
+
+class Language{
+  
+  private $lang_id;
+  private $lang_name;
+  
+  public function getLangId(){ return $this->lang_id; }  
+  public function setLangId($x){ $this->lang_id = $x; }
+  
+  public function getLangName(){ return $this->lang_name; }  
+  public function setLangName($x){ $this->lang_name = $x; }
+}
