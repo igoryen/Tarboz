3cd77626405ebe182672ffcc248d49e7194993fb
Habib <habibullah.zahoori@gmail.com>
2014-11-08 15:49:28 -0500

    Registration Form Completed

--- classes/BusinessLogicTier/language/LanguageManager.php
+++ classes/BusinessLogicTier/language/LanguageManager.php
@@ -3,9 +3,23 @@
 require_once DATA_ACCESSOR_DIR_LANG . 'LanguageDataAccessor.php';
 
 class LanguageManager{
-  public function getListOfLang(){
-    $lda = new LanguageDataAccessor();
-    $aryOfLang = $lda->getListOfLang();
-    return $aryOfLang;
+
+  public function getLanguages(){
+
+      $language = new LanguageDataAccessor();
+
+      $Language = $language->getLanguages();
+
+      return $Language;
   }
+
+  public function getLanguageByName($langname){
+
+  		$language = new LanguageDataAccessor();
+
+  		$lang_id = $language->getLanguageByName($langname);
+
+  		return $lang_id;
+  }
+  
 }
